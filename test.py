584c5b65ba7b3c3423b98af58a2df1d0e72f0b2b
Superblue342 <147359749+SuperBlue342@users.noreply.github.com>
2023-10-12 21:13:53 +0900

    zigzag php, python

--- test.py
+++ test.py
@@ -1,87 +1,139 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import requests
-from datetime import datetime
+#September 9, 2017
+#This is a supply and demand curve diagram with the axes ticks turned off. 
+#I will post it on the web in case others might find it useful. 
+#http://moneymarketsandmisperceptions.blogspot.com/2016/09/building-supply-and-demand-graphs-in.html
+#http://www.scipy-lectures.org/intro/matplotlib/matplotlib.html
+ 
+import matplotlib.pyplot as plt # for plot  diagrams
+import numpy as np  #for constructing vectors. 
+from sympy import *  #For solving out for the equilibrium price, etc. ,symbolically. 
 
 
-def get_ohlc_data(symbol, timeframe):
-    base_url = f'https://www.mexc.com/open/api/v2/market/kline?symbol={symbol}_USDT&interval={timeframe}m&limit=20'
-    
-    response = requests.get(base_url)
-    all_data = response.json()
-    
-    ohlc_data_list = []
-    
-    if response.status_code == 200 and all_data['code'] == 200:
-        for i in range(len(all_data['data'])):
-            timestamp = all_data['data'][i][0]
-            timestamp_datetime = datetime.utcfromtimestamp(timestamp)
-            formatted_date = timestamp_datetime.strftime('%Y-%m-%d %H:%M:%S')
-            close_price = float(all_data['data'][i][2])
-            high_price = float(all_data['data'][i][3])
-            low_price = float(all_data['data'][i][4])
-            
-            ohlc_data_list.append((formatted_date, high_price, low_price, close_price))
-    else:
-        print(f"API request failed with status code: {response.status_code}")
-        return None
-    
-    return ohlc_data_list
+  #We need to establish that Q is just a symbol, with no number value. 
+Q = Symbol('BTC')
+a1 = 2; a2=1; externality =3; Ps = a1+a2*Q; Psocial = a1 + externality + a2*Q; 
+a3 = 12; a4 =  1; Pd=  a3- a4*Q
 
-if __name__ == '__main__':
-    
-    symbol = 'XEC'
-    timeframe = '15'
-    time_ohlc_list = get_ohlc_data(symbol, timeframe)
-    
-    date_list = []
-    high_price_list = []
-    low_price_list = []
-    
-    if time_ohlc_list:
-        for time_ohlc in time_ohlc_list:
-            Date = time_ohlc[0]
-            high_price = time_ohlc[1]
-            low_price = time_ohlc[2]
-            date_list.append(Date)
-            high_price_list.append(high_price)
-            low_price_list.append(low_price)
+#Now we find the equilibrium quantity. The blank print is to skip a line for easier reading.
+Qeq=  solve( Ps-Pd,  Q) ; print(Qeq); print("   ")
+#But a solution to the solve command is a list, not a float number, so we convert.
+Qeq = float(Qeq[0])
+Peq= a1+a2*Qeq;  print("Peq = ", Peq, "Qeq= ", Qeq);print("   ")
 
-    data = {
-        'Date': date_list,
-        'High': high_price_list,
-        'Low': low_price_list
-    }
+#We also want the height of the S,   S+3, and D curves at the  equilibrium quantity. 
+PsQeq = a1 + a2*Qeq; PsocialQeq=  a1 + 3 + a2*Qeq; PdQeq=  a3- a4*Qeq
+print(("PsQeq= ", PsQeq , "PsocialQeq= ",PsocialQeq,  "PdQeq = ", PdQeq));print("   ")
 
-    df = pd.DataFrame(data)
+#Now we find the optimal quantity. 
+Qstar=  solve( Psocial-Pd,  Q) ; print(Qstar);print("   ")
+#But a solution to the solve command is a list, not a float number, so we convert.
+Qstar = float(Qstar[0])
+Pstar = a1+3+ a2*Qstar;  print("Pstar = ", Pstar, "Qstar = ", Qstar);print("   ")
 
-    # Parameters
-    swing_length = 10
-    history_to_keep = 20
+#We also want the height of the S,   S+3, and D curves at the   value-maximizing quantity. 
+PsQstar = a1  + a2*Qstar; PsocialQstar=  a1 + 3 + a2*Qstar; PdQstar=  a3- a4*Qstar
+print(("PsQstar = ", PsQstar , "PsocialQstar= ",PsocialQstar,  "PdQstar = ", PdQstar)); print("   ")
 
-    # Calculate Swing Highs and Lows
-    df['SwingHigh'] = df['High'].rolling(window=swing_length).max()
-    df['SwingLow'] = df['Low'].rolling(window=swing_length).min()
 
-    # Detect Supply and Demand Zones
-    df['SupplyZone'] = np.where(df['High'] == df['SwingHigh'], 1, 0)
-    df['DemandZone'] = np.where(df['Low'] == df['SwingLow'], -1, 0)
+##################################################################
 
-    # Create a new DataFrame to keep only the last 'history_to_keep' rows
-    df = df.tail(history_to_keep)
+#Now we start drawing the supply and demand curves. 
+plt.rcParams['lines.linewidth'] = 5 #This sets the default width of plot lines. 
+plt.rcParams['lines.color'] = 'black' #This is for straight lines. 
+plt.rcParams['axes.color_cycle']='black' #This is for plot lines, oddly enough. 
+#It is hard to find the names to set markerface color, for example. 
 
-    # Plotting
-    plt.figure(figsize=(12, 6))
-    plt.plot(df['Date'], df['High'], label='High', color='blue')
-    plt.plot(df['Date'], df['Low'], label='Low', color='red')
-    plt.scatter(df[df['SupplyZone'] == 1]['Date'], df[df['SupplyZone'] == 1]['High'], marker='^', color='green', label='Supply Zone')
-    plt.scatter(df[df['DemandZone'] == -1]['Date'], df[df['DemandZone'] == -1]['Low'], marker='v', color='orange', label='Demand Zone')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title(f'{symbol} Supply and Demand Zones')
-    plt.xticks(rotation=45)  # Rotate x-axis labels for readability
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()  # Ensure labels fit within the figure boundaries
-    plt.show()
+#We will have  a figure and axes, and we name them. 
+#Also, we say the figure will be big in size, so we can judge points better. 
+fig, ax= plt.subplots(figsize=(20, 10))
+ 
+#We want to get rid of the box around the diagram this way: 
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+#In putting on labels, we want a horizontal label on the y-axis as well as the x-axis.  
+plt.xlabel("Quantity", fontsize=28)
+plt.ylabel("Price        ", fontsize = 28, rotation = 0)
+
+#Here are the actual things to plot. 
+ #Q is now  a set of 200 points between  0 and  20 at which to sample the function plotted. 
+Q = np.linspace(0, 20, 200)
+ # We need to redefine our supply and demand equations (but the parameters are still OK)
+Ps = a1+a2*Q 
+Psocial = a1 + externality + a2*Q; 
+Pd=  a3- a4*Q;
+
+#Let's turn the x and y ticks off, since this is a qualitative diagram.
+plt.xticks([]); plt.yticks([]); 
+#We can turn on axis ticks for drawing help.   
+#plt.xticks(np.arange(0, max(Q)+1, 1.0))
+#plt.yticks(np.arange(0, max(Ps)+1, 1.0))
+
+    # Set x, y  limits for the points covered by the diagram. 
+plt.xlim(0, 6) 
+plt.ylim(0, 14)    
+
+#plt.plot is the object that has the actual plot. We will ahve three plots on one figure.
+ #I made the demadn curve look a little different. 
+plt.plot(Q, Ps    )
+plt.plot(Q, Pd, marker = 'o' )
+plt.plot(Q, Psocial )
+
+#Here is how to connect two points [x1,x2]  and  [y1,y2] with a line:
+plt.plot([Qstar, Qstar],[PsQstar,PsocialQstar], linewidth=1, color='black')
+plt.plot([Qeq, Qeq],[PsQeq,PsocialQeq], linewidth=1)
+
+ 
+
+#Put  dots (o)  at  [x1,x2]  and  [y1,y2]. Make sure they aren't clipped off at the axes. 
+plt.plot([0, 0, Qstar, Qstar, Qeq, Qeq],[PsQeq,PdQstar,   PdQstar,0,PsQeq,0],   'o',
+      markerfacecolor='black', markersize=12, clip_on=False,)
+
+
+# This next was supposed to fill in between the curves gray, but it leaves off a little bit.So I gave up. 
+#ax.fill_between(Q,  Ps, Psocial, where= Q<=Qstar,    facecolor='gray', interpolate=True)
+ 
+
+# Add  some text    to point (2,3) using Latex to get a subscript.
+plt.text(2,3 ,"$Supply, Q_s$", fontsize = 24)
+plt.text(2, 11,"$Demand, Q_d$", fontsize = 24)
+plt.text(4.8,  11,"$Supply + Externality$ of 3", fontsize = 20)
+plt.text(1,4,"A", fontsize = 32)
+plt.text(3.8, 7,"B", fontsize = 32)
+plt.text(4.2,  8,"C", fontsize = 32)
+
+#Turn the next line on to help with guessing where text should be. 
+#ax.minorticks_on(); ax.grid(which='both');
+ 
+#The annotate method  inserts an arrow and a phrase. 
+#It points the arrow at xy and puts the text xytext distance away. 
+plt.annotate(r'  $(Q_{unregulated},  P_{unregulated})$',
+             xy=(Qeq,PsQeq),  xycoords='data', 
+             xytext=(+10, +50), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+plt.annotate(r'  $Q_{unregulated}$',
+             xy=(Qeq,0),  xycoords='data', 
+             xytext=(+10, +50), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+plt.annotate(r'  $Q_{regulated}$',
+             xy=(Qstar,0),  xycoords='data', 
+             xytext=(+10, +50), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+plt.annotate(r'  $P_{unregulated}$',
+             xy=(0, PsQeq),  xycoords='data', 
+             xytext=(+10, +50), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+plt.annotate(r'  $P_{regulated}$',
+             xy=(0,PdQstar),  xycoords='data', 
+             xytext=(+10, +50), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+
+
+#The next command decides how much white space to put around the plot when it is saved. 
+plt.subplots_adjust(left=0.18, right=0.9, top=0.9,bottom= 0.15)
+plt.savefig("ans02-04.png") #Needs to be before plt.show()
+
+plt.show()
+ 
+plt.close("all")
--- zigzag.py
+++ zigzag.py
@@ -1,19 +1,63 @@
-def zigzag(s, c=0.05):
-    """ 
-    The Zig Zag indicator plots points on the chart whenever prices reverse by or greater than
-    a pre-chosen variable. Straight lines are then drawn, connecting these points. The indicator
-    is used to help identify price trends. It eliminates random price fluctuations and attempts
-    to show trend changes. Zig Zag lines only appear when there is a price movement between a
-    swing high and a swing low that is greater than a specified variable.
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-    >>> s1 = [0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6]
-    >>> zigzag(s1, c=5)
-    [7, 0]
-    >>> s2 = [0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6,
-    ...       5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4]
-    >>> zigzag(s2, c=5)
-    [7, 0, 6, 1, 9]
-    """    
+# get top 200 list
+def get_top_list(api_key, limit):
+    
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+    parameters = {
+        'start': '1',
+        'limit': limit,
+        'sort': 'market_cap',
+        'convert': 'USD',
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': api_key
+    }   
+
+    response = requests.get(url, headers=headers, params=parameters)
+
+    data = response.json()
+
+    if response.status_code == 200 and data['status']['error_code'] == 0:
+        return data['data']
+    else:
+        error_message = data['status']['error_message']
+        raise Exception(f"API request failed. Error: {error_message}")
+
+def get_ohlc_data(symbol, timeframe):
+    base_url = f'https://www.mexc.com/open/api/v2/market/kline?symbol={symbol}_USDT&interval={timeframe}m&limit=60'
+    response = requests.get(base_url)
+    all_data = response.json()
+    
+    ohlc_data_list = []
+    
+    if response.status_code == 200 and all_data['code'] == 200:
+        for data_point in all_data['data']:
+            timestamp = data_point[0]
+            timestamp_datetime = datetime.utcfromtimestamp(timestamp)
+            formatted_date = timestamp_datetime.strftime('%Y-%m-%d %H:%M:%S')
+            close_price = float(data_point[2])
+            high_price = float(data_point[3])
+            low_price = float(data_point[4])
+            
+            ohlc_data_list.append((formatted_date, high_price, low_price, close_price))
+    else:
+        print(f"API request failed with status code: {response.status_code}")
+        return None
+    
+    return ohlc_data_list
+
+def identify_supply_demand_zones(df):
+    demand_zone = df['Close'].idxmin(), df['Close'].min()
+    supply_zone = df['Close'].idxmax(), df['Close'].max()
+    return demand_zone, supply_zone
+
+def zigzag(s, c=0.05):   
     zz = []
     signal = 0
     inflection = s[0]
@@ -51,7 +95,106 @@ def zigzag(s, c=0.05):
                 continue
     return zz
 
+if __name__ == "__main__": 
+    api_key = '37b4a617-9609-48c6-8a41-d48db5b2ed44'
+    limit = 50
+    timeframe = '15'
+    
+    try:
+        cryptocurrencies = get_top_list(api_key, limit)
+        symbol_list = []
+        
+        for crypto in cryptocurrencies:
+            symbol = crypto['symbol']
+            symbol_list.append(symbol)
+            
+        zigzag_list = []
+        supply_list = []
+        demand_list = []
+        
+        for symbol in symbol_list:
+            ohlc_data_list = get_ohlc_data(symbol, timeframe)
+            if ohlc_data_list:
+                date_list = [data_point[0] for data_point in ohlc_data_list]
+                high_price_list = [data_point[1] for data_point in ohlc_data_list]
+                low_price_list = [data_point[2] for data_point in ohlc_data_list]
+                close_price_list = [data_point[3] for data_point in ohlc_data_list]
+            
+                data = {'Date': date_list, 'Close': close_price_list}
+                current_price = close_price_list[-1]
+                
+                #------------- Zigzag ---------------
+                s1 = data['Close']
+                # Generate ZigZag points for each corresponding date
+                points = zigzag(s1, c=0.0001)
+                
+                if len(points) > 0:
+                    # print(f"ZigZag Points: {points}")
+                    supply_point = points[0]
+                    demand_point = points[0]
+                    
+                    for point in points:
+                        if point > supply_point:
+                            supply_point = point
+                            
+                    for point in points:
+                        if point < demand_point:
+                            demand_point = point
+                    
+                    # print(f"{symbol}: supply_point: {supply_point}")
+                    # print(f"{symbol}: demand_point: {demand_point}")
+                    
+                    # print(f"{symbol}: current_price: {current_price}")
+                    
+                    if current_price <= demand_point:
+                        print(f"The {symbol} is in the demand zone")
+                        zigzag_list.append(symbol)
+                    elif supply_point <= current_price:
+                        print(f"The {symbol} is in the supply zone")
+                        zigzag_list.append(symbol)
+                #     else :
+                #         print(f"{symbol} is not in demand or supply zone")
+                # else:
+                #     print(f"No ZigZag points found for {symbol}")
+                
+                # -------- supply and demand zone
+                df = pd.DataFrame(data)
+                df['Date'] = pd.to_datetime(df['Date'])
+                df.set_index('Date', inplace=True)
+                
+                demand_zone, supply_zone = identify_supply_demand_zones(df)
+                if current_price <= demand_zone[1]:
+                    print(f"{symbol}: demand zone")
+                    demand_list.append(symbol)
+                elif current_price >= supply_zone[1]:
+                    print(f"{symbol}: supply zone")
+                    supply_list.append(symbol)
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+                # # Create a plot of the closing prices
+                # plt.figure(figsize=(10, 6))
+                # plt.plot(date_list, close_price_list, label='Close Price', color='blue')
+                
+                # # Plot ZigZag points for each corresponding date
+                # plt.scatter(date_list[:len(points)], points, color='red', marker='o', label='ZigZag Points')
+                # plt.axhline(y=demand_point, color='green', linestyle='--', label='Demand Zone')
+                # plt.axhline(y=supply_point, color='red', linestyle='--', label='Supply Zone')
+                
+                # plt.xlabel('Date')
+                # plt.ylabel('Price')
+                # plt.title(f'{symbol} Closing Prices with ZigZag Points')
+                # plt.xticks(rotation=45)
+                # plt.legend()
+                # plt.grid(True)
+                # plt.tight_layout()
+                
+                # # Display the plot
+                # plt.show()
+                
+        supply_compare_list = [item for item in zigzag_list if item in supply_list]
+        demand_compare_list = [item for item in zigzag_list if item in demand_list]
+        
+        print(f"supply_compare_list: {supply_compare_list}")
+        print(f"demand_compare_list: {demand_compare_list}")
+                
+    except Exception as e:
+        print(str(e))
